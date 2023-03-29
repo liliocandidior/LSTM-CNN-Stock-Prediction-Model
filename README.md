84a7193bc59d75e0dd932a9ad19a5f76cc875f14
YingzeHou <yhou63@wisc.edu>
2023-03-29 13:55:52 -0500

    YZH: Data Acquire First Try & README Project Specifications

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # LSTM-CNN-Stock-Prediction-Model
 
+To run the scripts, please start up a virtual environment using `venv`:
+-  run `python -m venv ./venv` to create the virtual environment named "venv" in the root directory
+- run `source ./venv/bin/activate` to activate the virtual environment to install all dependencies
+
+Required packages (dependencies) are in the file `requirements.txt`
+
+To install all required packages, please run `pip install -r requirements.txt` command line from the root folder, where the `requirements.txt` is located
+
 ## Initiative
 ### Construct and Compare Model Accuracy and Efficiency between 3 Stock Prediction Models
 
---
+++ data/data_acquire.py
@@ -0,0 +1,52 @@
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+'''
+Sample Args passed into get_data() function
+API_KEY: Fixed, I generated it via alpha_vantage platform
+TICKER: Variable, whichever stock we want to predict
+
+    API_KEY = 'R680A7OABBQ58NL3'
+    TICKER = 'AAPL'
+'''
+
+def create_dataset(data, n_past): 
+    x = []
+    y = []
+    for i in range(len(data)-n_past-1):
+        x.append(data[i:(i+n_past)])
+        y.append(data[i + n_past: i+n_past+1])
+    return np.array(x), np.array(y)
+
+def get_data(api_key, ticker):
+    ts = TimeSeries(api_key, output_format='pandas')
+
+    # Retrieve data from timeseries
+    data = ts.get_daily_adjusted(ticker, 'full')[0]
+
+    # Use close price & volumn as the feature space
+    data = data.iloc[:,[3,5]]
+
+    # Sort by ascending timestamps to match intuition
+    data = data.astype(float).sort_index(ascending=True, axis=0).tail(3000)
+
+    # Standarize the data, MinMaxScaler picked for stock price prediction (from online resources)
+    scaler=MinMaxScaler(feature_range=(0,1))
+    data_scaled = scaler.fit_transform(np.array(data))
+
+    # Split the data into x_data & y_data for training, testing, and prediction purpose
+    x_data, y_data = create_dataset(data_scaled, 20)
+
+    # Shuffle = false since we cannot mess up with the sequence for timeseries data
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.05, shuffle=False)
+    print(f'\n######################## Data for {ticker} scaled and partitioned complete ########################')
+    print(f'Sample Number: {str(x_data.shape[0])}; Train Sample Number: {str(x_train.shape[0])}; Test Sample Number: {str(x_test.shape[0])}')
+    print(f'###############################################################################################\n')
+
+    return (x_data, y_data, x_train, y_train, x_test, y_test)
+
+# if __name__ == '__main__':
+#     get_data(API_KEY, TICKER)

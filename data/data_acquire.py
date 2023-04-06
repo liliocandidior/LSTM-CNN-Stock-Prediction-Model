ceceb5c15738842ddac2bd3d7a11db6a723207d3
liliocandidior <107889719+liliocandidior@users.noreply.github.com>
2023-04-05 21:49:20 -0500

    Add files via upload

--- data/data_acquire.py
+++ data/data_acquire.py
@@ -13,13 +13,15 @@ TICKER: Variable, whichever stock we want to predict
     TICKER = 'AAPL'
 '''
 
-def create_dataset(data, n_past): 
+
+def create_dataset(data, n_past):
     x = []
     y = []
-    for i in range(len(data)-n_past-1):
-        x.append(data[i:(i+n_past)])
-        y.append(data[i + n_past: i+n_past+1])
+    for i in range(len(data) - n_past - 1):
+        x.append(data[i:(i + n_past)])
+        y.append(data[i + n_past: i + n_past + 1])
     return np.array(x), np.array(y)
+
 
 def get_data(api_key, ticker):
     ts = TimeSeries(api_key, output_format='pandas')
@@ -28,25 +30,35 @@ def get_data(api_key, ticker):
     data = ts.get_daily_adjusted(ticker, 'full')[0]
 
     # Use close price & volumn as the feature space
-    data = data.iloc[:,[3,5]]
+    data = data.iloc[:, [3, 5]]
 
     # Sort by ascending timestamps to match intuition
     data = data.astype(float).sort_index(ascending=True, axis=0).tail(3000)
 
     # Standarize the data, MinMaxScaler picked for stock price prediction (from online resources)
-    scaler=MinMaxScaler(feature_range=(0,1))
+    scaler = MinMaxScaler(feature_range=(0, 1))
     data_scaled = scaler.fit_transform(np.array(data))
 
     # Split the data into x_data & y_data for training, testing, and prediction purpose
     x_data, y_data = create_dataset(data_scaled, 20)
+    x_data = np.asarray([x_data[i][0] for i in range(len(x_data))])
+    x_data = x_data.reshape(-1, x_data.shape[-1])
+    y_data = y_data.reshape(-1, y_data.shape[-1])
 
     # Shuffle = false since we cannot mess up with the sequence for timeseries data
     x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.05, shuffle=False)
     print(f'\n######################## Data for {ticker} scaled and partitioned complete ########################')
-    print(f'Sample Number: {str(x_data.shape[0])}; Train Sample Number: {str(x_train.shape[0])}; Test Sample Number: {str(x_test.shape[0])}')
+    print(
+         f'Sample Number: {str(x_data.shape[0])}; Train Sample Number: {str(x_train.shape[0])}; Test Sample Number: {str(x_test.shape[0])}')
     print(f'###############################################################################################\n')
 
-    return (x_data, y_data, x_train, y_train, x_test, y_test)
+    return x_data, y_data, x_train, y_train, x_test, y_test
 
+
+API_KEY = 'R680A7OABBQ58NL3'
+TICKER = 'AAPL'
+dataset = get_data(API_KEY, TICKER)
 # if __name__ == '__main__':
 #     get_data(API_KEY, TICKER)
+
+

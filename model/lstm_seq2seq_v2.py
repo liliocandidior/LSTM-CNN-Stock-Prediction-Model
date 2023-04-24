80b143d10fe14d1f09d854c83da92ceb326dcbf3
YingzeHou <yhou63@wisc.edu>
2023-04-24 12:32:07 -0500

    Plot Util Included

--- model/lstm_seq2seq_v2.py
+++ model/lstm_seq2seq_v2.py
@@ -2,6 +2,9 @@ from keras.models import Sequential
 from keras.layers import Conv1D, MaxPooling1D, LSTM, Dense, RepeatVector
 from tensorflow import keras
 import matplotlib.pyplot as plt
+from pandas.tseries.offsets import CustomBusinessDay
+from pandas.tseries.holiday import USFederalHolidayCalendar
+import mplcursors
 from sklearn.metrics import mean_squared_error
 import sys
 sys.path.insert(0, 'data')
@@ -37,34 +40,22 @@ import time
 start = time.time()
 
 seq2seq_model.compile(loss='mean_absolute_error',optimizer=opt)
-seq2seq_model.fit(x_train, y_train, epochs=100, validation_data=(x_test,y_test), batch_size=64, verbose=1)
+seq2seq_model.fit(x_train, y_train, epochs=20, validation_data=(x_test,y_test), batch_size=64, verbose=1)
 
 end = time.time()
 print('Time: '+str(end - start))
 
-
-def calculate_accuracy(real, predict):
-    real = np.array(real) + 1
-    predict = np.array(predict) + 1
-    percentage = 1 - np.sqrt(np.mean(np.square((real - predict) / real)))
-    return percentage * 100
-
 train_predict = seq2seq_model.predict(x_train)
 train_predict=scaler.inverse_transform(train_predict[:, 0])
-x_train = scaler.inverse_transform(x_train)
 
 test_predict = seq2seq_model.predict(x_test)
 test_predict=scaler.inverse_transform(test_predict[:, 0])
-x_test = scaler.inverse_transform(x_test)
-
-acc = calculate_accuracy(x_test, test_predict)
 
 mse = math.sqrt(mean_squared_error(y_train,train_predict))
 mse2 = math.sqrt(mean_squared_error(y_test,test_predict))
 
 print(f'######################## Min Squared error training is {mse} ########################')
 print(f'######################## Min Squared error testing is {mse2} ########################')
-print(f'######################## Accuracy of training is {acc} ########################')
 
 plt.plot(train_predict, c='red', label = 'Prediction')
 plt.plot(x_train, c='black', label = 'Actual')

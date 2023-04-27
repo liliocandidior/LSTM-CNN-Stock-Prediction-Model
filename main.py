82e8dad26996d67e7e54c5a4c69484ff592d8024
YingzeHou <yhou63@wisc.edu>
2023-04-27 15:51:07 -0500

    Add Mode Choice

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from lstm_v2 import lstm
 API_KEY = 'R680A7OABBQ58NL3'
 def main(argv):
     global API_KEY
-    if len(argv) < 3:
-        print('Usage: python main.py <stock (AAPL, TSLA, etc..)> <model (lstm or seq2seq or cnn)>')
+    if len(argv) < 4:
+        print('Usage: python main.py <stock (AAPL, TSLA, etc..)> <model (lstm or seq2seq or cnn)> <mode (TRAINING or TESTING)>')
         sys.exit(1)
     # Update Mode and Stock
-    TICKER = argv[-2]
-    MODEL = argv[-1]
+    TICKER = argv[-3]
+    MODEL = argv[-2]
+    MODE = argv[-1]
 
     if MODEL == 'lstm':
-        lstm(API_KEY, TICKER)
+        lstm(API_KEY, TICKER, MODE)
     if MODEL == 'seq2seq':
         pass
     if MODEL == 'cnn':
